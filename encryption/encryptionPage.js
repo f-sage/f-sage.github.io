27172f7e0e639dcbc069c587d3435ab38ac8d119
f-sage <junia.en@gmail.com>
2021-03-13 17:13:34 +0200

    Encryption table now can be vertical; "no group can be created" message

--- encryption/encryptionPage.js
+++ encryption/encryptionPage.js
@@ -4,6 +4,7 @@ const functionsTable = document.getElementById('functionsTable');
 const keyPairsTable = document.getElementById("keyPairsTable");
 const groupList = document.getElementById("groupList");
 const groupTable= document.getElementById("group");
+const keyPairsHead=document.getElementById("keyPairsHead");
 
 functionsTable.style.visibility="hidden";
 keyPairsTable.style.visibility="hidden";
@@ -11,15 +12,31 @@ groupTable.style.visibility="hidden";
 let group;
 let encryptionModule;
 
+let width=window.innerWidth;
+window.onresize=function  FitTable(){
+    if(keyPairsTable.style.visibility === "hidden") return;
+    if(width===window.innerWidth) return;
+        console.log("Re-fitting the table...")
+        FormKeyPairsTable();
+
+}
+
 button.onclick = function() {
-    if(input.value=="") return;
+    if(input.value==""||input.value<1) return;
     encryptionModule=input.value;
-    group=new Group(EulerL(encryptionModule));
+    let groupModule=EulerL(encryptionModule);
+    console.log(EulerPhi(groupModule));
+    if(groupModule<2||EulerPhi(groupModule)<2||encryptionModule==1){
+        NoGroup();
+    }
+    else{
+    group=new Group(groupModule);
 
     FormFunctionsTable();
     FormGroup();
     FormKeyPairsTable();
     console.log(group.ToString());
+}
 };
 
 input.addEventListener("keyup", function(event) {
@@ -34,6 +51,7 @@ function FormFunctionsTable(){
     functionsTable.style.visibility = "initial";
     functionsTable.innerHTML="";
     //phi(N), L(N), phi(L(N)
+
     let row = functionsTable.insertRow();
     let eulerPhiN="φ(N)="+EulerPhi(encryptionModule);
     row.insertCell().innerHTML=eulerPhiN;
@@ -42,16 +60,15 @@ function FormFunctionsTable(){
     let eulerPhiL="φ(L(N))="+EulerPhi(EulerL(encryptionModule));
     row.insertCell().innerHTML=eulerPhiL;
 
-    console.log(eulerPhiN);
-    console.log(eulerLN);
-    console.log(eulerPhiL);
 }
 
 function FormKeyPairsTable(){
     keyPairsTable.style.visibility = "initial";
+    keyPairsHead.style.visibility="initial";
     keyPairsTable.innerHTML="";
+    width=window.innerWidth;
 
-    let head=keyPairsTable.createCaption();
+    let head=keyPairsHead;
     head.innerHTML="Possible key pairs:"
 
     //если инверс равен элементу не добавлять
@@ -66,44 +83,78 @@ function FormKeyPairsTable(){
             inverseElems.push(elem.inverse);
         }
     }
-
-    let columns=Math.ceil((window.innerWidth/2-140)/50);
-    let rows=Math.ceil(elems.length/(columns-1));
-    console.log("cols="+columns+", rows="+rows);
-
-    for(let i=0; i<rows;++i){
-        let tr=keyPairsTable.insertRow();
-        tr.insertCell().innerHTML="e";
-        for(let j=0; j<columns-1&&(i*(columns-1)+j)<elems.length;++j){
-            tr.insertCell().innerHTML=elems[i*(columns-1)+j];
-        }
-
-        tr=keyPairsTable.insertRow();
-        tr.insertCell().innerHTML="d";
-        for(let j=0; j<columns-1&&(i*(columns-1)+j)<elems.length;++j){
-            tr.insertCell().innerHTML=inverseElems[i*(columns-1)+j];
-        }
-        if(i!==rows-1) {
-            tr=keyPairsTable.insertRow();
-        }
+    if(elems.length===0) {head.innerHTML+=" none";
+    keyPairsTable.style.visibility="hidden";
     }
 
+    if(window.innerWidth>600) HorizontalKeyPairsTable(elems, inverseElems);
+    else VerticalKeyPairsTable(elems, inverseElems);
+
 }
+
+function HorizontalKeyPairsTable(elems, inverseElems) {
+    let columns = Math.ceil((window.innerWidth / 2 - 140) / 50);
+    let rows = Math.ceil(elems.length / (columns - 1));
+    console.log("cols=" + columns + ", rows=" + rows);
+
+    for (let i = 0; i < rows; ++i) {
+        let tr = keyPairsTable.insertRow();
+        tr.insertCell().innerHTML = "e";
+        for (let j = 0; j < columns - 1 && (i * (columns - 1) + j) < elems.length; ++j) {
+            tr.insertCell().innerHTML = elems[i * (columns - 1) + j];
+        }
+
+        tr = keyPairsTable.insertRow();
+        tr.insertCell().innerHTML = "d";
+        for (let j = 0; j < columns - 1 && (i * (columns - 1) + j) < elems.length; ++j) {
+            tr.insertCell().innerHTML = inverseElems[i * (columns - 1) + j];
+        }
+        if (i !== rows - 1) {
+            tr = keyPairsTable.insertRow();
+        }
+    }
+}
+
+function VerticalKeyPairsTable(elems, inverseElems){
+    let tr=keyPairsTable.insertRow();
+    tr.insertCell().innerHTML="e";
+    tr.insertCell().innerHTML="d";
+
+    for(let i=0;i<elems.length;++i){
+    tr=keyPairsTable.insertRow();
+    tr.insertCell().innerHTML=elems[i];
+    tr.insertCell().innerHTML=inverseElems[i];
+    }
+}
+
 
 function FormGroup(){
     groupTable.style.visibility="initial";
     let res="G<sup>*</sup><sub>"+group.elems.length+"</sub>=";
 
+    console.log(group.elems.length);
     res+="{";
-    for(let i=0;i<group.elems.length-1;++i){
-        if(group.elems[i].power>2) res+="<span class='coloured'>"+group.elems[i].elem+"</span>, ";
-        else res+=group.elems[i].elem+", ";
+    if(group.elems.length<2){
+        res+="1}";
     }
-    res+=group.elems[group.elems.length-1].elem+"}";
+    else {
+
+        for (let i = 0; i < group.elems.length - 1; ++i) {
+            if (group.elems[i].power > 2) res += "<span class='coloured'>" + group.elems[i].elem + "</span>, ";
+            else res += group.elems[i].elem + ", ";
+        }
+        res += group.elems[group.elems.length - 1].elem + "}";
+    }
 
     groupList.innerHTML=res;
 }
 
-function FitKeyPairsTable(){
 
+function NoGroup(){
+    functionsTable.style.visibility="hidden";
+    keyPairsTable.style.visibility="hidden";
+    keyPairsHead.style.visibility="hidden";
+    groupTable.style.visibility="initial";
+
+   groupList.innerHTML="No encryption group can be formed from that number." ;
 }
